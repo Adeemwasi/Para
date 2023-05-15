c455accb013911d6f9f28fe8a20def9b265603be
Adeemwasi <111539899+Adeemwasi@users.noreply.github.com>
2023-05-15 21:08:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var newsHeading = [
-  '<li class="item1 news-li"><a href="https://www.google.com">ProQual Level 6 NVQ diploma in Occupational Heath and Safety. <b style="color: red !important;">Click here.</b></a></li>',
-  '<li class="item2 news-li"><a href="https://www.google.com">A range of short courses in Occupational Health and Safety. <b style="color: red;">Click here.</b></a></li>',
-  '<li class="item3 news-li"><a href="https://www.google.com">Register with us as ISO management system auditors. <b style="color: red;">Click here.</b></a></li>',
+  '<li class="item1 news-li"><a href="https://www.google.com">ProQual Level 6 NVQ diploma in Occupational Heath and Safety. <b style="color: #222831 !important;">Click here.</b></a></li>',
+  '<li class="item2 news-li"><a href="https://www.google.com">A range of short courses in Occupational Health and Safety. <b style="color: #222831;">Click here.</b></a></li>',
+  '<li class="item3 news-li"><a href="https://www.google.com">Register with us as ISO management system auditors. <b style="color: #222831;">Click here.</b></a></li>',
 ];
 var currentIndex = 0;
 var animationInterval;
@@ -80,10 +80,32 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-  var items = $(".navbar, .logo, .banner");
+  var items = $(".banner");
   var interval = 1000;
 
   setInterval(function () {
-    items.toggleClass(".pressed");
+    items.toggleClass("pressed");
   }, interval);
+});
+
+
+$(document).ready(function () {
+  var items = $(".img-left, .img-right");
+  var interval = 1000;
+
+  setInterval(function () {
+    items.toggleClass("pressed2");
+  }, interval);
+});
+
+$(document).ready(function () {
+  var items = $(".list, .logo, .main-img");
+
+  items.mouseenter(function () {
+    $(this).addClass("pressed");
+  });
+
+  items.mouseleave(function () {
+    $(this).removeClass("pressed");
+  });
 });
